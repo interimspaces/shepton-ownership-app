3460976f464186a71e72c552173e9d0d8473a050
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-23 23:41:14 +0100

    Prepare for Heroku Deployment

--- public/script.js
+++ public/script.js
@@ -2,7 +2,7 @@ window.addEventListener('DOMContentLoaded', async () => {
     const mapContainer = document.querySelector('.building-objects');
 
     // Fetch the SVG file
-    const response = await fetch('../data/buildings.svg');
+    const response = await fetch('/data/buildings.svg');
     const svgText = await response.text();
 
     // Create a temporary container to parse the SVG
@@ -10,11 +10,19 @@ window.addEventListener('DOMContentLoaded', async () => {
     tempContainer.innerHTML = svgText;
     const svgElement = tempContainer.querySelector('svg');
 
-    // Add a class to the polygons
+    // Add interactivity to the polygons
     svgElement.querySelectorAll('polygon').forEach((path) => {
-        path.classList.add('building-object');
+        path.classList.add('building-object'); // Adding the class
+        path.addEventListener('mouseover', (e) => {
+            e.target.style.stroke = '#ff6600';
+            e.target.style.strokeWidth = '6px';
+        });
+        path.addEventListener('mouseout', (e) => {
+            e.target.style.stroke = '';
+            e.target.style.strokeWidth = '';
+        });
     });
 
-    // Append the SVG to the map container
+    // Append the SVG to the building objects container
     mapContainer.appendChild(svgElement);
 });
