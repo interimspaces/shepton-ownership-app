2bf16995d280d26a0cf9e4a7974e56a4df000c33
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-23 21:16:29 +0100

    Prepare for Heroku Deployment

--- public/script.js
+++ public/script.js
@@ -1,37 +1,18 @@
-window.addEventListener('DOMContentLoaded', async () => {
-    const mapContainer = document.querySelector('.map-container');
-    const img = document.createElement('img');
-    img.src = 'images/shepton-background-map.png'; // Re-include the map image src
-    img.alt = 'Map';
-    mapContainer.appendChild(img);
+window.addEventListener('DOMContentLoaded', () => {
+    const svgObject = document.getElementById('buildings');
+    svgObject.addEventListener('load', () => {
+        const svgDoc = svgObject.contentDocument;
+        const paths = svgDoc.querySelectorAll('path'); // Assuming buildings are represented as path elements
 
-    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    svg.id = 'buildings';
-    svg.setAttribute('viewBox', '0 0 800 600'); // Set viewBox based on your map's dimensions
-    mapContainer.appendChild(svg);
-
-    const response = await fetch('/data/building-outlines.json');
-    const buildingOutlines = await response.json();
-
-    buildingOutlines.forEach((building) => {
-        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
-        path.setAttribute('d', convertToPath(building)); // No need for .coordinates
-        path.setAttribute('fill', 'neutralgray'); // You can replace 'neutralgray' with an actual color code
-        path.addEventListener('click', (e) => {
-            handleBuildingClick(building, e);
+        paths.forEach((path) => {
+            path.addEventListener('click', (e) => {
+                handleBuildingClick(path, e);
+            });
         });
-        svg.appendChild(path);
     });
 });
 
-function convertToPath(coordinates) {
-    const d = coordinates.map((coord, index) => {
-        const command = index === 0 ? 'M' : 'L';
-        return `${command} ${coord[0]} ${coord[1]}`;
-    }).join(' ');
-    return d + ' Z';
-}
-
 function handleBuildingClick(building, e) {
     // Code to handle click event on building, e.g., show a modal to modify data
+    alert('Building clicked!'); // Example action
 }
