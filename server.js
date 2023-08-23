d57d1bcdc05c6eb1775548c732812477d91fa664
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-23 14:25:41 +0100

    Prepare for Heroku Deployment

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const app = express();
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use('/data', express.static(path.join(__dirname, 'data')));
+
 
 // Send index.html for all routes
 app.get('*', (req, res) => {
