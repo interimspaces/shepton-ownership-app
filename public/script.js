5fc54e2482cad39f03bd8e3246bc2a03a14baab0
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-23 22:54:40 +0100

    Prepare for Heroku Deployment

--- public/script.js
+++ public/script.js
@@ -1,20 +1,43 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    // Path to your SVG file
-    const svgPath = 'images/buildings.svg';
-    
-    // Select the container where you want to place the SVG
-    const buildingObjectsContainer = document.querySelector('.building-objects');
+    const mapContainer = document.querySelector('.map-container');
+    const img = document.createElement('img');
+    // img.src = 'images/shepton-background-map.png';
+    img.alt = 'Map';
+    mapContainer.appendChild(img);
 
-    // Fetch the SVG file
-    const response = await fetch(svgPath);
-    const svgText = await response.text();
+    const svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+    svg.id = 'buildings';
+    mapContainer.appendChild(svg);
 
-    // Parse the fetched SVG text
-    const parser = new DOMParser();
-    const svgDoc = parser.parseFromString(svgText, 'image/svg+xml');
+    const response = await fetch('/data/buildings.svg');
+    const buildingOutlines = await response.json();
 
-    // Append the parsed SVG to the container
-    buildingObjectsContainer.appendChild(svgDoc.documentElement);
-    
-    // Optionally, you can now add event listeners or manipulate the SVG as needed
+    buildingOutlines.forEach((building) => {
+        const path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.setAttribute('d', convertToPath(building.coordinates));
+        path.setAttribute('fill', 'neutralgray');
+        path.setAttribute('class', 'building-object'); // Added line to set CSS class
+        path.addEventListener('click', (e) => {
+            handleBuildingClick(building, e);
+        });
+        path.addEventListener('mouseover', (e) => { // Added mouseover event listener
+            e.target.style.fill = '#ff0000'; // Change to desired hover color
+        });
+        path.addEventListener('mouseout', (e) => { // Added mouseout event listener
+            e.target.style.fill = 'neutralgray'; // Change back to original color
+        });
+        svg.appendChild(path);
+    });
 });
+
+function convertToPath(coordinates) {
+    const d = coordinates.map((coord, index) => {
+        const command = index === 0 ? 'M' : 'L';
+        return `${command} ${coord[0]} ${coord[1]}`;
+    }).join(' ');
+    return d + ' Z';
+}
+
+function handleBuildingClick(building, e) {
+    // Code to handle click event on building, e.g., show a modal to modify data
+}
