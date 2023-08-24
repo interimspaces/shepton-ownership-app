500525aadc248cebe3b0bb66a8fa734ecd605959
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-24 10:35:52 +0100

    Added zoom on map

--- public/script.js
+++ public/script.js
@@ -1,15 +1,52 @@
 window.addEventListener('DOMContentLoaded', async () => {
+    const zoomingArea = document.querySelector('.zooming-area');
+    const zoomInButton = document.getElementById('zoomIn');
+    const zoomOutButton = document.getElementById('zoomOut');
     const mapContainer = document.querySelector('.building-objects');
-    const backgroundMap = document.querySelector('.background-map');
+    let zoomLevel = 1;
+
+    const zoomIn = () => {
+        zoomLevel += 0.1;
+        if (zoomLevel > 2) zoomLevel = 2;
+        zoomingArea.style.transformOrigin = '0 0';
+        zoomingArea.style.transform = `scale(${zoomLevel})`;
+    };
+
+    const zoomOut = () => {
+        zoomLevel -= 0.1;
+        if (zoomLevel < 1) zoomLevel = 1;
+        zoomingArea.style.transformOrigin = '0 0';
+        zoomingArea.style.transform = `scale(${zoomLevel})`;
+    };
+
+    zoomInButton.addEventListener('mousedown', () => {
+        zoomInInterval = setInterval(zoomIn, 100);
+    });
+
+    zoomInButton.addEventListener('mouseup', () => {
+        clearInterval(zoomInInterval);
+    });
+
+    zoomOutButton.addEventListener('mousedown', () => {
+        zoomOutInterval = setInterval(zoomOut, 100);
+    });
+
+    zoomOutButton.addEventListener('mouseup', () => {
+        clearInterval(zoomOutInterval);
+    });
+
+    // Fetch the SVG file
     const response = await fetch('/data/buildings.svg');
     const svgText = await response.text();
+
+    // Create a temporary container to parse the SVG
     const tempContainer = document.createElement('div');
     tempContainer.innerHTML = svgText;
     const svgElement = tempContainer.querySelector('svg');
-    mapContainer.appendChild(svgElement);
 
+    // Add interactivity to the polygons
     svgElement.querySelectorAll('polygon').forEach((path) => {
-        path.classList.add('building-object');
+        path.classList.add('building-object'); // Adding the class
         path.addEventListener('mouseover', (e) => {
             e.target.style.stroke = '#ff6600';
             e.target.style.strokeWidth = '6px';
@@ -20,21 +57,6 @@ window.addEventListener('DOMContentLoaded', async () => {
         });
     });
 
-    const zoomInButton = document.getElementById('zoomIn');
-    const zoomOutButton = document.getElementById('zoomOut');
-    let zoomLevel = 1;
-
-    zoomInButton.addEventListener('click', () => {
-        zoomLevel += 0.1;
-        if (zoomLevel > 2) zoomLevel = 2;
-        mapContainer.style.transform = `scale(${zoomLevel})`;
-        backgroundMap.style.transform = `scale(${zoomLevel})`; // Also zoom the background
-    });
-
-    zoomOutButton.addEventListener('click', () => {
-        zoomLevel -= 0.1;
-        if (zoomLevel < 1) zoomLevel = 1;
-        mapContainer.style.transform = `scale(${zoomLevel})`;
-        backgroundMap.style.transform = `scale(${zoomLevel})`; // Also zoom the background
-    });
+    // Append the SVG to the building objects container
+    mapContainer.appendChild(svgElement);
 });
