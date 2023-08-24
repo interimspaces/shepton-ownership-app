dcc84894831967d4508366e9070aebae64916df6
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-24 09:03:19 +0100

    Added zoom on map

--- public/script.js
+++ public/script.js
@@ -1,32 +1,13 @@
 window.addEventListener('DOMContentLoaded', async () => {
     const mapContainer = document.querySelector('.map-container');
+    const backgroundMap = document.querySelector('.background-map');
+    const buildingObjects = document.querySelector('.building-objects');
 
     // Fetch the SVG file
     const response = await fetch('/data/buildings.svg');
     const svgText = await response.text();
-
-    // Create a temporary container to parse the SVG
-    const tempContainer = document.createElement('div');
-    tempContainer.innerHTML = svgText;
-    const svgElement = tempContainer.querySelector('svg');
-
-    // Add interactivity to the polygons
-    svgElement.querySelectorAll('polygon').forEach((path) => {
-        path.classList.add('building-object');
-        path.addEventListener('mouseover', (e) => {
-            e.target.style.stroke = '#ff6600';
-            e.target.style.strokeWidth = '6px';
-            e.target.style.filter = 'drop-shadow(0 4px 8px rgba(0, 0, 0, 0.2))';
-        });
-        path.addEventListener('mouseout', (e) => {
-            e.target.style.stroke = '#999999';
-            e.target.style.strokeWidth = '0px';
-            e.target.style.filter = 'drop-shadow(0 0px 0px rgba(0, 0, 0, 0))';
-        });
-    });
-
-    // Append the SVG to the building objects container
-    mapContainer.appendChild(svgElement);
+    buildingObjects.innerHTML = svgText;
+    const svgElement = buildingObjects.querySelector('svg');
 
     // Map zoom
     const zoomInButton = document.getElementById('zoomIn');
@@ -36,14 +17,21 @@ window.addEventListener('DOMContentLoaded', async () => {
     zoomInButton.addEventListener('click', () => {
         zoomLevel += 0.1;
         if (zoomLevel > 2) zoomLevel = 2;
-        mapContainer.style.transformOrigin = '0 0';
-        mapContainer.style.transform = `scale(${zoomLevel})`;
+        svgElement.style.transformOrigin = '0 0';
+        backgroundMap.style.transformOrigin = '0 0';
+        svgElement.style.transform = `scale(${zoomLevel})`;
+        backgroundMap.style.transform = `scale(${zoomLevel})`;
     });
 
     zoomOutButton.addEventListener('click', () => {
         zoomLevel -= 0.1;
         if (zoomLevel < 1) zoomLevel = 1;
-        mapContainer.style.transformOrigin = '0 0';
-        mapContainer.style.transform = `scale(${zoomLevel})`;
+        svgElement.style.transformOrigin = '0 0';
+        backgroundMap.style.transformOrigin = '0 0';
+        svgElement.style.transform = `scale(${zoomLevel})`;
+        backgroundMap.style.transform = `scale(${zoomLevel})`;
     });
-});
+
+    // Add interactivity to the polygons
+    svgElement.querySelectorAll('polygon').forEach((path) => {
+       
