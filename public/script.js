ec7a4c9bc2eba86db62c1026e0b30db2a6fec2c9
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-24 08:44:47 +0100

    Added zoom on map

--- public/script.js
+++ public/script.js
@@ -25,57 +25,33 @@ window.addEventListener('DOMContentLoaded', async () => {
 
     // Append the SVG to the building objects container
     mapContainer.appendChild(svgElement);
-});
 
-// modal window for building mouseover
-svgElement.querySelectorAll('polygon').forEach((path) => {
-    path.addEventListener('mouseover', (e) => {
-        e.target.style.fill = '#ff6600';
+    // Modal window for building mouseover
+    svgElement.querySelectorAll('polygon').forEach((path) => {
+        path.addEventListener('mouseover', (e) => {
+            e.target.style.fill = '#ff6600';
 
-        // Get the modal element
-        const modal = document.getElementById('buildingModal');
+            // Get the modal element
+            const modal = document.getElementById('buildingModal');
 
-        // Show the modal
-        modal.style.display = 'block';
+            // Show the modal
+            modal.style.display = 'block';
 
-        // Set the position based on the cursor
-        modal.style.left = e.clientX + 'px';
-        modal.style.top = e.clientY + 'px';
+            // Set the position based on the cursor
+            modal.style.left = e.clientX + 'px';
+            modal.style.top = e.clientY + 'px';
 
-        // Set the address (this will be dynamic later)
-        document.getElementById('buildingAddress').innerText = 'Address here';
+            // Set the address (this will be dynamic later)
+            document.getElementById('buildingAddress').innerText = 'Address here';
+        });
+
+        path.addEventListener('mouseout', (e) => {
+            e.target.style.fill = '#999999';
+
+            // Hide the modal
+            document.getElementById('buildingModal').style.display = 'none';
+        });
     });
 
-    path.addEventListener('mouseout', (e) => {
-        e.target.style.fill = '#999999';
-
-        // Hide the modal
-        document.getElementById('buildingModal').style.display = 'none';
-    });
-});
-
-
-// map zoom
-window.addEventListener('DOMContentLoaded', async () => {
-    // ... rest of your existing code ...
-
-    // map zoom
-    const zoomInButton = document.getElementById('zoomIn');
-    const zoomOutButton = document.getElementById('zoomOut');
-    const mapContainer = document.querySelector('.map-container');
-    let zoomLevel = 1;
-
-    zoomInButton.addEventListener('click', () => {
-        zoomLevel += 0.1;
-        if (zoomLevel > 2) zoomLevel = 2; // Set max zoom level
-        mapContainer.style.transformOrigin = '0 0'; // Anchor the zoom to the top left
-        mapContainer.style.transform = `scale(${zoomLevel})`;
-    });
-
-    zoomOutButton.addEventListener('click', () => {
-        zoomLevel -= 0.1;
-        if (zoomLevel < 1) zoomLevel = 1; // Set min zoom level
-        mapContainer.style.transformOrigin = '0 0'; // Anchor the zoom to the top left
-        mapContainer.style.transform = `scale(${zoomLevel})`;
-    });
-});
+    // Map zoom
+    const zoomInButton
