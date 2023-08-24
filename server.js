0d5d9eb0aa3a2d2cd11f127f87ff09f7ca61b533
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-24 22:38:48 +0100

    Function deploy

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const app = express();
 
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/data', express.static(path.join(__dirname, 'data')));
 
 app.use(express.json());
 
@@ -26,6 +24,17 @@ app.get('/properties', (req, res) => {
     }
     res.status(200).json(results.rows);
   });
+});
+
+// Get individual property by ID
+app.get('/properties/:id', (req, res) => {
+    const id = req.params.id;
+    pool.query('SELECT * FROM PropertyTable WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json(results.rows[0]);
+    });
 });
 
 // Update a property
@@ -53,15 +62,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-// Get individual property by ID
-app.get('/properties/:id', (req, res) => {
-    const id = req.params.id;
-    pool.query('SELECT * FROM PropertyTable WHERE id = $1', [id], (error, results) => {
-      if (error) {
-        throw error;
-      }
-      res.status(200).json(results.rows[0]);
-    });
-  });
-  
