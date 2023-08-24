5565d6a72b9f95615115d6d3d1b15aefe5f5aa3f
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-24 09:00:05 +0100

    Added zoom on map

--- public/script.js
+++ public/script.js
@@ -1,32 +1,34 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    const mapContainer = document.querySelector('.map-container'); // Target the whole map container for zooming
-    const buildingObjects = document.querySelector('.building-objects');
+    const mapContainer = document.querySelector('.map-container');
+
+    // Fetch the SVG file
     const response = await fetch('/data/buildings.svg');
     const svgText = await response.text();
+
+    // Create a temporary container to parse the SVG
     const tempContainer = document.createElement('div');
     tempContainer.innerHTML = svgText;
     const svgElement = tempContainer.querySelector('svg');
 
+    // Add interactivity to the polygons
     svgElement.querySelectorAll('polygon').forEach((path) => {
         path.classList.add('building-object');
         path.addEventListener('mouseover', (e) => {
             e.target.style.stroke = '#ff6600';
             e.target.style.strokeWidth = '6px';
-            const modal = document.getElementById('buildingModal');
-            modal.style.display = 'block';
-            modal.style.left = e.clientX + 'px';
-            modal.style.top = e.clientY + 'px';
-            document.getElementById('buildingAddress').innerText = 'Address here';
+            e.target.style.filter = 'drop-shadow(0 4px 8px rgba(0, 0, 0, 0.2))';
         });
         path.addEventListener('mouseout', (e) => {
-            e.target.style.stroke = '';
-            e.target.style.strokeWidth = '';
-            document.getElementById('buildingModal').style.display = 'none';
+            e.target.style.stroke = '#999999';
+            e.target.style.strokeWidth = '0px';
+            e.target.style.filter = 'drop-shadow(0 0px 0px rgba(0, 0, 0, 0))';
         });
     });
 
-    buildingObjects.appendChild(svgElement); // Appending SVG to the building objects container
+    // Append the SVG to the building objects container
+    mapContainer.appendChild(svgElement);
 
+    // Map zoom
     const zoomInButton = document.getElementById('zoomIn');
     const zoomOutButton = document.getElementById('zoomOut');
     let zoomLevel = 1;
@@ -34,12 +36,14 @@ window.addEventListener('DOMContentLoaded', async () => {
     zoomInButton.addEventListener('click', () => {
         zoomLevel += 0.1;
         if (zoomLevel > 2) zoomLevel = 2;
-        mapContainer.style.transform = `scale(${zoomLevel})`; // Applying zoom to the entire map container
+        mapContainer.style.transformOrigin = '0 0';
+        mapContainer.style.transform = `scale(${zoomLevel})`;
     });
 
     zoomOutButton.addEventListener('click', () => {
         zoomLevel -= 0.1;
         if (zoomLevel < 1) zoomLevel = 1;
-        mapContainer.style.transform = `scale(${zoomLevel})`; // Applying zoom to the entire map container
+        mapContainer.style.transformOrigin = '0 0';
+        mapContainer.style.transform = `scale(${zoomLevel})`;
     });
 });
