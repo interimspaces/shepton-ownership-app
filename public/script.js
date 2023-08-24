dc95508fdf5759c816ac28548bf4a5f2a3f31d41
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-24 09:10:03 +0100

    Added zoom on map

--- public/script.js
+++ public/script.js
@@ -1,15 +1,25 @@
 window.addEventListener('DOMContentLoaded', async () => {
-    const mapContainer = document.querySelector('.map-container');
+    const mapContainer = document.querySelector('.building-objects');
     const backgroundMap = document.querySelector('.background-map');
-    const buildingObjects = document.querySelector('.building-objects');
-
-    // Fetch the SVG file
     const response = await fetch('/data/buildings.svg');
     const svgText = await response.text();
-    buildingObjects.innerHTML = svgText;
-    const svgElement = buildingObjects.querySelector('svg');
+    const tempContainer = document.createElement('div');
+    tempContainer.innerHTML = svgText;
+    const svgElement = tempContainer.querySelector('svg');
+    mapContainer.appendChild(svgElement);
 
-    // Map zoom
+    svgElement.querySelectorAll('polygon').forEach((path) => {
+        path.classList.add('building-object');
+        path.addEventListener('mouseover', (e) => {
+            e.target.style.stroke = '#ff6600';
+            e.target.style.strokeWidth = '6px';
+        });
+        path.addEventListener('mouseout', (e) => {
+            e.target.style.stroke = '';
+            e.target.style.strokeWidth = '';
+        });
+    });
+
     const zoomInButton = document.getElementById('zoomIn');
     const zoomOutButton = document.getElementById('zoomOut');
     let zoomLevel = 1;
@@ -17,21 +27,14 @@ window.addEventListener('DOMContentLoaded', async () => {
     zoomInButton.addEventListener('click', () => {
         zoomLevel += 0.1;
         if (zoomLevel > 2) zoomLevel = 2;
-        svgElement.style.transformOrigin = '0 0';
-        backgroundMap.style.transformOrigin = '0 0';
-        svgElement.style.transform = `scale(${zoomLevel})`;
-        backgroundMap.style.transform = `scale(${zoomLevel})`;
+        mapContainer.style.transform = `scale(${zoomLevel})`;
+        backgroundMap.style.transform = `scale(${zoomLevel})`; // Also zoom the background
     });
 
     zoomOutButton.addEventListener('click', () => {
         zoomLevel -= 0.1;
         if (zoomLevel < 1) zoomLevel = 1;
-        svgElement.style.transformOrigin = '0 0';
-        backgroundMap.style.transformOrigin = '0 0';
-        svgElement.style.transform = `scale(${zoomLevel})`;
-        backgroundMap.style.transform = `scale(${zoomLevel})`;
+        mapContainer.style.transform = `scale(${zoomLevel})`;
+        backgroundMap.style.transform = `scale(${zoomLevel})`; // Also zoom the background
     });
-
-    // Add interactivity to the polygons
-    svgElement.querySelectorAll('polygon').forEach((path) => {
-       
+});
