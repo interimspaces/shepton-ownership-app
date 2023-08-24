d8845adecdeee9da614b70ce0e224c8b3a08e71f
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-24 08:50:04 +0100

    Added zoom on map

--- public/script.js
+++ public/script.js
@@ -1,57 +1,46 @@
 window.addEventListener('DOMContentLoaded', async () => {
     const mapContainer = document.querySelector('.building-objects');
-
-    // Fetch the SVG file
     const response = await fetch('/data/buildings.svg');
     const svgText = await response.text();
-
-    // Create a temporary container to parse the SVG
     const tempContainer = document.createElement('div');
     tempContainer.innerHTML = svgText;
     const svgElement = tempContainer.querySelector('svg');
 
-    // Add interactivity to the polygons
     svgElement.querySelectorAll('polygon').forEach((path) => {
-        path.classList.add('building-object'); // Adding the class
+        path.classList.add('building-object');
         path.addEventListener('mouseover', (e) => {
             e.target.style.stroke = '#ff6600';
             e.target.style.strokeWidth = '6px';
+            const modal = document.getElementById('buildingModal');
+            modal.style.display = 'block';
+            modal.style.left = e.clientX + 'px';
+            modal.style.top = e.clientY + 'px';
+            document.getElementById('buildingAddress').innerText = 'Address here';
         });
         path.addEventListener('mouseout', (e) => {
             e.target.style.stroke = '';
             e.target.style.strokeWidth = '';
-        });
-    });
-
-    // Append the SVG to the building objects container
-    mapContainer.appendChild(svgElement);
-
-    // Modal window for building mouseover
-    svgElement.querySelectorAll('polygon').forEach((path) => {
-        path.addEventListener('mouseover', (e) => {
-            e.target.style.fill = '#ff6600';
-
-            // Get the modal element
-            const modal = document.getElementById('buildingModal');
-
-            // Show the modal
-            modal.style.display = 'block';
-
-            // Set the position based on the cursor
-            modal.style.left = e.clientX + 'px';
-            modal.style.top = e.clientY + 'px';
-
-            // Set the address (this will be dynamic later)
-            document.getElementById('buildingAddress').innerText = 'Address here';
-        });
-
-        path.addEventListener('mouseout', (e) => {
-            e.target.style.fill = '#999999';
-
-            // Hide the modal
             document.getElementById('buildingModal').style.display = 'none';
         });
     });
 
-    // Map zoom
-    const zoomInButton
+    mapContainer.appendChild(svgElement);
+
+    const zoomInButton = document.getElementById('zoomIn');
+    const zoomOutButton = document.getElementById('zoomOut');
+    let zoomLevel = 1;
+
+    zoomInButton.addEventListener('click', () => {
+        zoomLevel += 0.1;
+        if (zoomLevel > 2) zoomLevel = 2;
+        mapContainer.style.transformOrigin = '0 0';
+        mapContainer.style.transform = `scale(${zoomLevel})`;
+    });
+
+    zoomOutButton.addEventListener('click', () => {
+        zoomLevel -= 0.1;
+        if (zoomLevel < 1) zoomLevel = 1;
+        mapContainer.style.transformOrigin = '0 0';
+        mapContainer.style.transform = `scale(${zoomLevel})`;
+    });
+});
