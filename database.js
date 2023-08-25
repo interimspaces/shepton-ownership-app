fce99c4b81d46cb76fdf036717b4f2f146149a9a
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-25 22:02:39 +0100

    Function deploy

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const pool = require('./database.js');
 
 const pool = new Pool({
-  connectionString: process.env.shepton-ownership-app, // Replace with your Heroku PostgreSQL URL
+  connectionString: process.env.shepton-ownership-app-60f62ab2a676.herokuapp.com/, // Replace with your Heroku PostgreSQL URL
   ssl: {
     rejectUnauthorized: false
   }
