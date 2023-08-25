74f601a2414827aed799f789cb7394e1447242a8
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-25 12:11:23 +0100

    NewFunct

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.get('/properties', (req, res) => {
 // Get individual property by ID
 app.get('/properties/:id', (req, res) => {
     const id = req.params.id;
-    pool.query('SELECT * FROM PropertyTable WHERE id = $1', [id], (error, results) => {
+    pool.query('SELECT * FROM PropertyTable WHERE PropertyID = $1', [id], (error, results) => {
       if (error) {
         throw error;
       }
@@ -39,18 +39,63 @@ app.get('/properties/:id', (req, res) => {
 });
 
 // Update a property
-app.put('/properties/:id', (req, res) => {
-  // Code to update property in the database
-});
-
-// Delete a property
-app.delete('/properties/:id', (req, res) => {
-  // Code to delete property from the database
+app.put('/properties/:id', async (req, res) => {
+  const { id } = req.params;
+  const propertyData = req.body.property;
+  const query = `
+    UPDATE PropertyTable SET
+    PropertySVGID = $1,
+    PropertyNumber = $2,
+    PropertyName = $3,
+    PropertyStreet = $4,
+    PropertyTown = $5,
+    PropertyPostCode = $6,
+    PropertyWardLocation = $7,
+    PropertyType = $8,
+    PropertyCategory = $9,
+    PropertyTenant = $10,
+    PropertyOccupationStatus = $11,
+    PropertyListedStatus = $12,
+    PropertyConservationZone = $13,
+    PropertyNotes = $14,
+    PropertyPurchaseDate = $15,
+    PropertyPurchaseAmount = $16
+    WHERE PropertyID = $17;
+  `;
+  try {
+    await pool.query(query, Object.values(propertyData).concat(id));
+    res.status(200).json({ message: 'Property updated successfully' });
+  } catch (error) {
+    res.status(400).json({ error: 'Could not update property' });
+  }
 });
 
 // Add a new owner
-app.post('/owners', (req, res) => {
-  // Code to insert a new owner into the database
+app.post('/properties/:id/ownership', async (req, res) => {
+  const { id } = req.params;
+  const ownershipData = req.body;
+  const query = `
+    INSERT INTO OwnershipTable (PropertyID, ResolvedOwner, FreeholdOwner, LeaseholdOwner)
+    VALUES ($1, $2, $3, $4);
+  `;
+  try {
+    await pool.query(query, [id, ...Object.values(ownershipData)]);
+    res.status(201).json({ message: 'Ownership added successfully' });
+  } catch (error) {
+    res.status(400).json({ error: 'Could not add ownership' });
+  }
+});
+
+// Delete ownership history
+app.delete('/ownership/:ownerId', async (req, res) => {
+  const { ownerId } = req.params;
+  const query = 'DELETE FROM OwnershipTable WHERE OwnerID = $1;';
+  try {
+    await pool.query(query, [ownerId]);
+    res.status(200).json({ message: 'Ownership deleted successfully' });
+  } catch (error) {
+    res.status(400).json({ error: 'Could not delete ownership' });
+  }
 });
 
 // Send index.html for all other routes
