30215f714e386b7fae9e1c2e64540106029a3a1f
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-26 21:40:32 +0100

    NewFunct

--- public/js/client.js
+++ public/js/client.js
@@ -45,110 +45,110 @@ window.addEventListener('DOMContentLoaded', async () => {
   zoomOutButton.addEventListener('mouseup', () => {
     clearInterval(zoomOutInterval);
   });
+  // Drag functionality
+  zoomingArea.addEventListener('mousedown', (e) => {
+    isDragging = true;
+    prevX = e.clientX;
+    prevY = e.clientY;
+  });
 
-    // Drag functionality
-    zoomingArea.addEventListener('mousedown', (e) => {
-      isDragging = true;
+  window.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+      let dx = e.clientX - prevX;
+      let dy = e.clientY - prevY;
+      zoomingArea.scrollLeft -= dx;
+      zoomingArea.scrollTop -= dy;
       prevX = e.clientX;
       prevY = e.clientY;
-    });
-  
-    window.addEventListener('mousemove', (e) => {
-      if (isDragging) {
-        let dx = e.clientX - prevX;
-        let dy = e.clientY - prevY;
-        zoomingArea.scrollLeft -= dx;
-        zoomingArea.scrollTop -= dy;
-        prevX = e.clientX;
-        prevY = e.clientY;
-      }
-    });
-  
-    window.addEventListener('mouseup', () => {
-      isDragging = false;
-    });
-  
-    // Fetch SVG and add event listeners
-    const response = await fetch('./data/buildings.svg');
-    if (!response.ok) {
-      console.error(`Error fetching the SVG: ${response.status}, ${response.statusText}`);
-      return;
     }
-    const svgText = await response.text();
-    const tempContainer = document.createElement('div');
-    tempContainer.innerHTML = svgText;
-    const svgElement = tempContainer.querySelector('svg');
-    const propertyDataResponse = await fetch('/properties');
-    const allProperties = await propertyDataResponse.json();
-  
-    svgElement.querySelectorAll('polygon').forEach((polygon) => {
-      polygon.classList.add('building-object');
-  
-      const correspondingProperty = allProperties.find((property) => property.propertysvgid === polygon.id);
-      if (correspondingProperty) {
-        polygon.dataset.propertyId = correspondingProperty.propertyid;
+  });
+
+  window.addEventListener('mouseup', () => {
+    isDragging = false;
+  });
+
+  // Fetch SVG and add event listeners
+  const response = await fetch('./data/buildings.svg');
+  if (!response.ok) {
+    console.error(`Error fetching the SVG: ${response.status}, ${response.statusText}`);
+    return;
+  }
+  const svgText = await response.text();
+  const tempContainer = document.createElement('div');
+  tempContainer.innerHTML = svgText;
+  const svgElement = tempContainer.querySelector('svg');
+
+  const propertyDataResponse = await fetch('/properties');
+  const allProperties = await propertyDataResponse.json();
+
+  svgElement.querySelectorAll('polygon').forEach((polygon) => {
+    polygon.classList.add('building-object');
+
+    const correspondingProperty = allProperties.find((property) => property.propertyid === polygon.id);
+    if (correspondingProperty) {
+      polygon.dataset.propertyId = correspondingProperty.propertyid;
+    }
+
+    polygon.addEventListener('click', async (e) => {
+      if (activePolygon) {
+        activePolygon.classList.remove('active');
       }
-  
-      polygon.addEventListener('click', async (e) => {
-        if (activePolygon) {
-          activePolygon.classList.remove('active');
-        }
-  
-        activePolygon = e.target;
-        activePolygon.classList.add('active');
-  
-        const propertyId = activePolygon.dataset.propertyId;
-        if (!propertyId) return;
-  
-        document.getElementById('PropertySVGID').value = activePolygon.id;
-        const propertyResponse = await fetch(`/properties/${propertyId}`);
-        const propertyData = await propertyResponse.json();
-        
-        fields.forEach((field) => {
-          const key = field.getAttribute('data-key');
-          field.value = propertyData[key] || '';
-          field.disabled = true;
-        });
-  
-        editButton.disabled = false;
-        saveButton.disabled = true;
-      });
-    });
-  
-    mapContainer.appendChild(svgElement);
-  
-    // Edit and Save functionalities
-    editButton.addEventListener('click', () => {
-      fields.forEach((field) => {
-        field.disabled = false;
-      });
-      editButton.disabled = true;
-      saveButton.disabled = false;
-    });
-  
-    saveButton.addEventListener('click', async () => {
+
+      activePolygon = e.target;
+      activePolygon.classList.add('active');
+
       const propertyId = activePolygon.dataset.propertyId;
-      const propertyData = {};
-  
+      if (!propertyId) return;
+
+      document.getElementById('PropertyID').value = activePolygon.id;
+
+      const propertyResponse = await fetch(`/properties/${propertyId}`);
+      const propertyData = await propertyResponse.json();
+      
       fields.forEach((field) => {
         const key = field.getAttribute('data-key');
-        propertyData[key] = field.value;
-      });
-  
-      const response = await fetch(`/properties/${propertyId}`, {
-        method: 'PUT',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify(propertyData)
-      });
-  
-      const result = await response.json();
-      console.log(result.message);
-  
-      fields.forEach((field) => {
+        field.value = propertyData[key] || '';
         field.disabled = true;
       });
+
       editButton.disabled = false;
       saveButton.disabled = true;
     });
   });
-      
+
+  mapContainer.appendChild(svgElement);
+
+  // Edit and Save functionalities
+  editButton.addEventListener('click', () => {
+    fields.forEach((field) => {
+      field.disabled = false;
+    });
+    editButton.disabled = true;
+    saveButton.disabled = false;
+  });
+
+  saveButton.addEventListener('click', async () => {
+    const propertyId = activePolygon.dataset.propertyId;
+    const propertyData = {};
+
+    fields.forEach((field) => {
+      const key = field.getAttribute('data-key');
+      propertyData[key] = field.value;
+    });
+
+    const response = await fetch(`/properties/${propertyId}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(propertyData)
+    });
+
+    const result = await response.json();
+    console.log(result.message);
+
+    fields.forEach((field) => {
+      field.disabled = true;
+    });
+    editButton.disabled = false;
+    saveButton.disabled = true;
+  });
+});
