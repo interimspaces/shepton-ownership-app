9e1bfb3c32f2f280771fcff288e139ed75bc0424
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-27 12:02:26 +0100

    NewFunct

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString = "postgres://kcavgsmupzkxzi:72f1c5875dc878c63ece38475e7b345d9aa531eea9e297c4a68a71a68d8e141f@ec2-44-215-40-87.compute-1.amazonaws.com:5432/dao96376jl1cte";
   ssl: {
     rejectUnauthorized: false
   }
