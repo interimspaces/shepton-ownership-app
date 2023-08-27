dbb42f90d0b72132031bd7a4e7b311ebf2b7ed42
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-08-27 10:15:19 +0100

    NewFunct

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Hardcoded connection string as a workaround
-const connectionString = "postgres://kcavgsmupzkxzi:72f1c5875dc878c63ece38475e7b345d9aa531eea9e297c4a68a71a68d8e141f@HOST:5432/dao96376jl1cte";
+const connectionString = "postgres://kcavgsmupzkxzi:72f1c5875dc878c63ece38475e7b345d9aa531eea9e297c4a68a71a68d8e141f@ec2-44-215-40-87.compute-1.amazonaws.com:5432/dao96376jl1cte";
 
 const pool = new Pool({
   connectionString: connectionString,
