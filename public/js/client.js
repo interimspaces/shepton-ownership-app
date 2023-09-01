64c51b4a34e8affb66077c07d44bd0e522644ea8
Dan Simon <Dan@Dans-MacBook-Pro.local>
2023-09-01 13:37:00 +0100

    Update database

--- public/js/client.js
+++ public/js/client.js
@@ -1,49 +1,55 @@
-async function fetchSVG(svgPath) {
-    const response = await fetch(svgPath);
-    const svgText = await response.text();
-    const tempContainer = document.createElement('div');
-    tempContainer.innerHTML = svgText;
-    return tempContainer.querySelector('svg');
-  }
-  
-  async function fetchPropertyData(apiUrl) {
-    const response = await fetch(apiUrl);
-    return await response.json();
-  }
-  
-  function addInteractivity(svgElement, propertyData) {
-    const polygons = svgElement.querySelectorAll('polygon');
-    polygons.forEach((polygon, index) => {
-      polygon.classList.add('building');
-      const data = propertyData[index];
-      for (const [key, value] of Object.entries(data)) {
-        polygon.dataset[key] = value;
+window.addEventListener('DOMContentLoaded', async () => {
+  // DOM Elements
+  const mapContainer = document.querySelector('.building-objects');
+  const editButton = document.getElementById('editProperty');
+  const saveButton = document.getElementById('saveProperty');
+  const fields = document.querySelectorAll('.field');
+  const propertysvgidField = document.getElementById('propertysvgid'); // Added
+
+  // State variables
+  let activePolygon;
+
+  // Fetch SVG and add event listeners
+  const svgResponse = await fetch('./data/buildings.svg');
+  const svgText = await svgResponse.text();
+  const tempContainer = document.createElement('div');
+  tempContainer.innerHTML = svgText;
+  const svgElement = tempContainer.querySelector('svg');
+
+  const propertyDataResponse = await fetch('/properties');
+  const allProperties = await propertyDataResponse.json();
+
+  svgElement.querySelectorAll('polygon').forEach((polygon) => {
+    polygon.classList.add('building-object');
+    const correspondingProperty = allProperties.find((property) => property.propertyid === polygon.id);
+    if (correspondingProperty) {
+      polygon.dataset.propertyId = correspondingProperty.propertyid;
+      polygon.dataset.propertysvgid = correspondingProperty.propertysvgid; // Added
+    }
+
+    polygon.addEventListener('click', async (e) => {
+      if (activePolygon) {
+        activePolygon.classList.remove('active');
       }
-      polygon.addEventListener('click', function() {
-        document.querySelectorAll('.active').forEach(el => el.classList.remove('active'));
-        polygon.classList.add('active');
-        populateFields(data);
-        disableFields();
-        enableEditButton();
+      activePolygon = e.target;
+      activePolygon.classList.add('active');
+      const propertyId = activePolygon.dataset.propertyId;
+      if (!propertyId) return;
+      document.getElementById('PropertyID').value = activePolygon.id;
+      propertysvgidField.value = activePolygon.dataset.propertysvgid; // Added
+
+      const propertyResponse = await fetch(`/properties/${propertyId}`);
+      const propertyData = await propertyResponse.json();
+      fields.forEach((field) => {
+        const key = field.getAttribute('data-key');
+        field.value = propertyData[key] || '';
+        field.disabled = true;
       });
+      editButton.disabled = false;
+      saveButton.disabled = true;
     });
-  }
-  
-  function populateFields(data) {
-    // Populate specific fields with property data
-  }
-  
-  function disableFields() {
-    // Disable the fields
-  }
-  
-  function enableEditButton() {
-    // Enable the edit button
-  }
-  
-  async function initMap(svgPath, apiUrl, mapContainerId) {
-    const svgElement = await fetchSVG(svgPath);
-    const propertyData = await fetchPropertyData(apiUrl);
-    addInteractivity(svgElement, propertyData);
-    document.getElementById(mapContainerId).appendChild(svgElement);
-  }
+  });
+
+  mapContainer.appendChild(svgElement);
+
+});
--- server.js
+++ server.js
@@ -1,29 +1,68 @@
+// Required Modules
 const express = require('express');
 const path = require('path');
-const { Client } = require('pg');
+const { Pool } = require('pg');
+const { generatePolygonID } = require('./database'); // Import the new function
 
+// Initialisation
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Hardcoded connection string as a workaround
+const connectionString = 'postgres://kcavgsmupzkxzi:72f1c5875dc878c63ece38475e7b345d9aa531eea9e297c4a68a71a68d8e141f@ec2-44-215-40-87.compute-1.amazonaws.com:5432/dao96376jl1cte';
 
-app.get('/api/property-data', async (req, res) => {
-  const client = new Client({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false
-    }
-  });
-  await client.connect();
-  const result = await client.query('SELECT * FROM properties');
-  await client.end();
-  res.json(result.rows);
+const pool = new Pool({
+  connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
-app.get('/api/svg', (req, res) => {
-  res.sendFile(path.join(__dirname, 'path/to/your/svg/file.svg'));
+// Function to assign permanent IDs to polygons (removed)
+
+pool.connect((err, client, done) => {
+  if (err) return console.error(err);
+  console.log('Connected to PostgreSQL');
+  done();
 });
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware for parsing JSON data
+app.use(express.json());
+
+// Error handling middleware function
+const errorHandler = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal Server Error' });
+};
+
+// Main Page Route
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// GET All Polygons
+app.get('/polygons', async (req, res, next) => {
+  try {
+    const { rows } = await pool.query('SELECT * FROM svgelements');
+    const updatedRows = rows.map(row => {
+      if (!row.svg_id) {
+        row.svg_id = generatePolygonID();
+        // Update database here
+      }
+      return row;
+    });
+    res.status(200).json(updatedRows);
+  } catch (err) {
+    next(err);
+  }
+});
+
+// Existing routes...
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
